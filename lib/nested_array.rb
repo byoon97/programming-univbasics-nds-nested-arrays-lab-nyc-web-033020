8ef4e9684473e11bcf5a1ac6a6f858f3916f636a
Brandon Yoon <byoon5397@gmail.com>
2020-03-23 19:13:48 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -17,7 +17,7 @@ CONVENTIONAL_PRODUCE = [
 ]
 
 def assembled_matrix
-  produce_storage_room = [CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE]
+  produce_storage_room = [[CONVENTIONAL_PRODUCE], [ORGANIC_PRODUCE]]
   return produce_storage_room
 end
 
